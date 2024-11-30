1983b6883cd18a0f5b8281b77d7162e38ef0e601
Mariusz Kryński <mrk@sed.pl>
2024-11-30 21:47:36 +0100

    cleanup

--- components/canbus_udp_multicast/canbus.py
+++ components/canbus_udp_multicast/canbus.py
@@ -6,16 +6,9 @@ from esphome.const import CONF_ID, CONF_TRIGGER_ID
 from esphome.components.esp32 import add_idf_sdkconfig_option
 from esphome.components.canbus import (
     CANBUS_SCHEMA,
-    CONF_ON_FRAME,
     CONF_CANBUS_ID,
-    CONF_CAN_ID,
-    CONF_CAN_ID_MASK,
-    CONF_USE_EXTENDED_ID,
-    CONF_REMOTE_TRANSMISSION_REQUEST,
     CanbusComponent,
-    CanbusTrigger,
     register_canbus,
-    validate_id,
 )
 
 CONF_MULTICAST_IP = "multicast_ip"
@@ -36,19 +29,6 @@ CONFIG_SCHEMA = CANBUS_SCHEMA.extend({
     cv.Optional(CONF_MULTICAST_IP, default="232.10.11.12"): cv.All(cv.ipv4, cv.string),
     cv.Optional(CONF_MULTICAST_PORT, default=43113): cv.int_range(min=0, max=65535),
     cv.Optional(CONF_IF_KEY, default="WIFI_STA_DEF"): cv.string,
-
-    cv.Optional(CONF_ON_FRAME): automation.validate_automation(
-        {
-            cv.GenerateID(CONF_TRIGGER_ID): cv.declare_id(CanbusTrigger),
-            cv.Required(CONF_CAN_ID): cv.int_range(min=0, max=0x1FFFFFFF),
-            cv.Optional(CONF_CAN_ID_MASK, default=0x1FFFFFFF): cv.int_range(
-                min=0, max=0x1FFFFFFF
-            ),
-            cv.Optional(CONF_USE_EXTENDED_ID, default=False): cv.boolean,
-            cv.Optional(CONF_REMOTE_TRANSMISSION_REQUEST): cv.boolean,
-        },
-        validate_id,
-    ),
 })
 
 
@@ -66,28 +46,4 @@ async def to_code(config):
         config[CONF_IF_KEY],
     )
     await register_canbus(canbus_udp_multicast, config)
-
-    for conf in config.get(CONF_ON_FRAME, []):
-        can_id = conf[CONF_CAN_ID]
-        can_id_mask = conf[CONF_CAN_ID_MASK]
-        ext_id = conf[CONF_USE_EXTENDED_ID]
-        trigger = cg.new_Pvariable(
-            conf[CONF_TRIGGER_ID], canbus, can_id, can_id_mask, ext_id
-        )
-        if CONF_REMOTE_TRANSMISSION_REQUEST in conf:
-            cg.add(
-                trigger.set_remote_transmission_request(
-                    conf[CONF_REMOTE_TRANSMISSION_REQUEST]
-                )
-            )
-        await cg.register_component(trigger, conf)
-        await automation.build_automation(
-            trigger,
-            [
-                (cg.std_vector.template(cg.uint8), "x"),
-                (cg.uint32, "can_id"),
-                (cg.bool_, "remote_transmission_request"),
-            ],
-            conf,
-        )
     add_idf_sdkconfig_option("CONFIG_LWIP_SO_RCVBUF", True)
